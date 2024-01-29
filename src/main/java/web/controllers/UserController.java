a632625504e4205f69e9ae03d44aef0cd126e86f
EgorLum <grggvzd@gmail.com>
2024-01-29 17:13:20 +0300

    5.5 commit

--- src/main/java/web/controllers/UserController.java
+++ src/main/java/web/controllers/UserController.java
@@ -41,33 +41,42 @@ public class UserController {
 //        return "";
 //    }
     @GetMapping("/new")
-    public String getNewUserForm(@ModelAttribute("user") User user) {
+//    public String getNewUserForm(@ModelAttribute("user") User user) {
+    public String newUser(Model model) {
+        model.addAttribute("user", new User());
+        System.out.println("get mapping new "+model.getAttribute("user"));
         return "/new_user";
     }
 
     @PostMapping("/new")
     public String creatNewUser(@ModelAttribute User user) {
+//        System.out.println("post mapping new "+user);
         userService.addUser(user);
+        System.out.println("post mapping new "+user);
         return "redirect:/users";
     }
 
 
     @GetMapping("/edit")
     public ModelAndView editUserForm(@RequestParam Integer id) {
+        System.out.println("get mapping edit "+id);
         ModelAndView mav = new ModelAndView("/edit_user");
         User user = userService.getUserById(id);
+        System.out.println("get mapping edit "+user);
         mav.addObject("user", user);
         return mav;
     }
 
     @PostMapping("/edit")
     public String editUser(@ModelAttribute("user") User user) {
+        System.out.println("post mapping edit "+user);
         userService.updateUser(user);
         return "redirect:/users";
     }
 
     @PostMapping("/del_user")
     public String deleteUser(@RequestParam("id") Integer id) {
+        System.out.println("post mapping del "+id);
         userService.deleteUserById(id);
         return "redirect:/users";
     }
